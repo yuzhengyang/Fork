02905d7fd3025dc75914f6547163a35156f086d5
yuzhengyang <inc@live.cn>
2016-10-22 10:37:37 +0800

    创建工具类

---
+++ Fork.Net/Y.Controls/Loadings/SimpleLoading.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Y.Controls.Loadings
+{
+    partial class SimpleLoading
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleLoading));
+            this.PnMain = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PnMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnMain
+            // 
+            this.PnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(173)))), ((int)(((byte)(238)))));
+            this.PnMain.Controls.Add(this.pictureBox1);
+            this.PnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnMain.Location = new System.Drawing.Point(0, 0);
+            this.PnMain.Name = "PnMain";
+            this.PnMain.Size = new System.Drawing.Size(643, 351);
+            this.PnMain.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(200, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SimpleLoading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PnMain);
+            this.Name = "SimpleLoading";
+            this.Size = new System.Drawing.Size(643, 351);
+            this.Load += new System.EventHandler(this.SimpleLoading_Load);
+            this.PnMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnMain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Fork.Net/Y.Controls/Loadings/SimpleLoading.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Y.Controls.Loadings
+{
+    public partial class SimpleLoading : UserControl
+    {
+        private int _opacity = 125;
+
+        public SimpleLoading()
+        {
+            InitializeComponent();
+        }
+
+        private void SimpleLoading_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        [Bindable(true), Category("Custom"), DefaultValue(125), Description("背景的透明度. 有效值0-255")]
+        public int Opacity
+        {
+            get { return _opacity; }
+            set
+            {
+                if (value > 255) value = 255;
+                else if (value < 0) value = 0;
+                _opacity = value;
+                this.Invalidate();
+            }
+        }
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            if (this._opacity > 0)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(this._opacity, this.BackColor)),
+                                         this.ClientRectangle);
+            }
+        }
+        public void Show() {
+             
+        }
+    }
+}
