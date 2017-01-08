637e7d60cc86294e24d4f6a68dd4d59e1cf2753f
yuzhengyang <inc@live.cn>
2017-01-08 23:02:30 +0800

    添加日期转换

---
+++ Fork.Net/Y.Utils/TimeUtils/DateTimeConvert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y.Utils.TimeUtils
+{
+    public sealed class DateTimeConvert
+    {
+        /// <summary>
+        /// yyyy-MM-dd HH:mm:ss
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string ToStandardString(DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
