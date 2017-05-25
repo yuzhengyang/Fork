9aa9dd94197be4e1580e8216236dcce119804404
yuzhengyang <inc@live.cn>
2017-05-25 17:53:17 +0800

    新建模块化更新

---
+++ Fork.Net/Oreo.Update/Models/VersionFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreo.Update.Models
+{
+    public class VersionFile
+    {
+        public string File { get; set; }
+    }
+}
