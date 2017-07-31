efdd0cc860d99f3361d91cdc0dc6b3b5820be819
yuzhengyang <inc@live.cn>
2017-07-31 16:37:51 +0800

    添加Form唯一控制工具（特殊需求）

---
+++ Fork.Net/Test/Y.Test/Commons/R.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Y.Utils.DataUtils.Collections;
+
+namespace Y.Test.Commons
+{
+    public static class R
+    {
+        public static FormDictionaryTool FD = new FormDictionaryTool();
+
+        public static void Test()
+        {
+            FD.Get();
+        }
+    }
+}
