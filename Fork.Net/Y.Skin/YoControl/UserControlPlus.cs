81520f53faebfdd06424d314f636d7f50c36892c
yuzhengyang <inc@live.cn>
2017-08-30 16:32:23 +0800

    添加自定义控件（可Invoke操作控件的visible和enable）

---
+++ Fork.Net/Y.Skin/YoControl/UserControlPlus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Y.Skin.YoControl
+{
+    public partial class UserControlPlus : UserControl
+    {
+        public UserControlPlus()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControlPlus_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void UIVisible(Control ctrl, bool visible = true)
+        {
+            Invoke(new Action(() =>
+            {
+                ctrl.Visible = visible;
+            }));
+        }
+        protected void UIEnable(Control ctrl, bool enable = true)
+        {
+            Invoke(new Action(() =>
+            {
+                ctrl.Enabled = enable;
+            }));
+        }
+    }
+}
