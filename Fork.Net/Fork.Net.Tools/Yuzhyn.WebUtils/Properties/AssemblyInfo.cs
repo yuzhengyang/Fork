69a9cd8cbeffff171ebec797bcb8c54d61e184cf
yuzhengyang <inc@live.cn>
2017-10-09 18:33:02 +0800

    工具类整体目录修改

--- Fork.Net/Fork.Net.Tools/Yuzhyn.WebUtils/Properties/AssemblyInfo.cs
+++ Fork.Net/Fork.Net.Tools/Yuzhyn.WebUtils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Y.FileQueryEngine")]
+[assembly: AssemblyTitle("Yuzhyn.WebUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Y.FileQueryEngine")]
+[assembly: AssemblyProduct("Yuzhyn.WebUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("497f570c-8398-458d-970c-dfa3dd9fe746")]
+[assembly: Guid("61fd5609-8d6d-434b-9a90-7719d45df8d2")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
