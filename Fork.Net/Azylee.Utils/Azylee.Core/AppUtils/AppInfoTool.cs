c02d77324d0b199e7152c102dd0013f33a123ab8
yuzhengyang <inc@live.cn>
2018-05-17 23:34:54 +0800

    优化读取APP的CPU使用获取方法，调整字节单位转换方法名称

--- Fork.Net/Azylee.Utils/Azylee.Core/AppUtils/AppInfoTool.cs
+++ Fork.Net/Azylee.Utils/Azylee.Core/AppUtils/AppInfoTool.cs
@@ -54,13 +54,47 @@ namespace Azylee.Core.AppUtils
         /// <param name="end"></param>
         /// <param name="interval"></param>
         /// <returns></returns>
+        [Obsolete]
         public static double CalcCpuRate(Process process, TimeSpan begin, int interval)
         {
-            //当前时间
-            var current = process.TotalProcessorTime;
-            //间隔时间内的CPU运行时间除以逻辑CPU数量
-            var minus = current - begin;
-            double value = minus.TotalMilliseconds / Environment.ProcessorCount / interval * 100;
+            double value = 0;
+            try
+            {
+                //当前时间
+                var current = process.TotalProcessorTime;
+                //间隔时间内的CPU运行时间除以逻辑CPU数量
+                var minus = current - begin;
+                value = minus.TotalMilliseconds / Environment.ProcessorCount / interval * 100;
+            }
+            catch { }
+
+            if (value < 0) return 0;
+            if (100 < value) return 100;
+            return value;
+        }
+        /// <summary>
+        /// 计算CPU占用率（自动刷新TimeSpan）
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="begin"></param>
+        /// <param name="end"></param>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public static double CalcCpuRate(Process process, ref TimeSpan begin, int interval)
+        {
+            double value = 0;
+            try
+            {
+                //当前时间
+                var current = process.TotalProcessorTime;
+                //间隔时间内的CPU运行时间除以逻辑CPU数量
+                var minus = current - begin;
+                value = minus.TotalMilliseconds / Environment.ProcessorCount / interval * 100;
+
+                begin = process.TotalProcessorTime;
+            }
+            catch { }
+
             if (value < 0) return 0;
             if (100 < value) return 100;
             return value;
@@ -89,17 +123,6 @@ namespace Azylee.Core.AppUtils
             try
             {
                 p = Process.GetProcessById(id);
-                value = p.WorkingSet64 / 1024;
-            }
-            catch { }
-            finally { p?.Dispose(); }
-            return value;
-        }
-        public static long RAM(Process p)
-        {
-            long value = 0;
-            try
-            {
                 value = p.WorkingSet64 / 1024;
             }
             catch { }
