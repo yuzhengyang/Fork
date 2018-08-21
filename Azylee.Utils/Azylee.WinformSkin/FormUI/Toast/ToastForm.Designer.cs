5bfb76c3bf93c676f5e05cc5da2076a3ad468dd1
yuzhengyang <inc@live.cn>
2018-08-21 15:53:53 +0800

    Toast提示框增加文本长度自动增长支持

--- Azylee.Utils/Azylee.WinformSkin/FormUI/Toast/ToastForm.Designer.cs
+++ Azylee.Utils/Azylee.WinformSkin/FormUI/Toast/ToastForm.Designer.cs
@@ -35,41 +35,52 @@
             this.TMHide = new System.Windows.Forms.Timer(this.components);
             this.TMHideAnim = new System.Windows.Forms.Timer(this.components);
             this.TMShowAnim = new System.Windows.Forms.Timer(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.PNMain = new System.Windows.Forms.Panel();
+            this.PNContainer = new System.Windows.Forms.Panel();
+            this.PNText = new System.Windows.Forms.Panel();
+            this.PNTitle = new System.Windows.Forms.Panel();
+            this.PNIcon = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.PNMain.SuspendLayout();
+            this.PNContainer.SuspendLayout();
+            this.PNText.SuspendLayout();
+            this.PNTitle.SuspendLayout();
+            this.PNIcon.SuspendLayout();
             this.SuspendLayout();
             // 
             // PBIcon
             // 
             this.PBIcon.BackColor = System.Drawing.Color.Transparent;
+            this.PBIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.PBIcon.Image = global::Azylee.WinformSkin.Properties.Resources.toast_warning;
-            this.PBIcon.Location = new System.Drawing.Point(16, 16);
+            this.PBIcon.Location = new System.Drawing.Point(13, 16);
             this.PBIcon.Name = "PBIcon";
             this.PBIcon.Size = new System.Drawing.Size(42, 41);
-            this.PBIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PBIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.PBIcon.TabIndex = 4;
             this.PBIcon.TabStop = false;
             this.PBIcon.Click += new System.EventHandler(this.PBIcon_Click);
             // 
             // LBTitle
             // 
+            this.LBTitle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.LBTitle.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.LBTitle.ForeColor = System.Drawing.Color.White;
-            this.LBTitle.Location = new System.Drawing.Point(68, 7);
+            this.LBTitle.Location = new System.Drawing.Point(3, 3);
             this.LBTitle.Name = "LBTitle";
-            this.LBTitle.Size = new System.Drawing.Size(270, 20);
+            this.LBTitle.Size = new System.Drawing.Size(288, 20);
             this.LBTitle.TabIndex = 5;
             this.LBTitle.Text = "测试标题测试标题测试标题测试标题";
             this.LBTitle.Click += new System.EventHandler(this.LBTitle_Click);
             // 
             // LBText
             // 
+            this.LBText.Dock = System.Windows.Forms.DockStyle.Fill;
             this.LBText.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.LBText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.LBText.Location = new System.Drawing.Point(68, 30);
+            this.LBText.Location = new System.Drawing.Point(3, 3);
             this.LBText.Name = "LBText";
-            this.LBText.Size = new System.Drawing.Size(270, 44);
+            this.LBText.Size = new System.Drawing.Size(288, 52);
             this.LBText.TabIndex = 6;
             this.LBText.Text = "测试，内容。测试，内容。测试，内容。测试，内容。测试，内容。测试，内容。";
             this.LBText.Click += new System.EventHandler(this.LBText_Click);
@@ -88,16 +99,55 @@
             this.TMShowAnim.Interval = 10;
             this.TMShowAnim.Tick += new System.EventHandler(this.TMShowAnim_Tick);
             // 
-            // panel1
+            // PNMain
             // 
-            this.panel1.Controls.Add(this.PBIcon);
-            this.panel1.Controls.Add(this.LBTitle);
-            this.panel1.Controls.Add(this.LBText);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(360, 80);
-            this.panel1.TabIndex = 7;
+            this.PNMain.Controls.Add(this.PNContainer);
+            this.PNMain.Controls.Add(this.PNIcon);
+            this.PNMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PNMain.Location = new System.Drawing.Point(0, 0);
+            this.PNMain.Name = "PNMain";
+            this.PNMain.Padding = new System.Windows.Forms.Padding(3);
+            this.PNMain.Size = new System.Drawing.Size(360, 90);
+            this.PNMain.TabIndex = 8;
+            // 
+            // PNContainer
+            // 
+            this.PNContainer.Controls.Add(this.PNText);
+            this.PNContainer.Controls.Add(this.PNTitle);
+            this.PNContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PNContainer.Location = new System.Drawing.Point(63, 3);
+            this.PNContainer.Name = "PNContainer";
+            this.PNContainer.Size = new System.Drawing.Size(294, 84);
+            this.PNContainer.TabIndex = 10;
+            // 
+            // PNText
+            // 
+            this.PNText.Controls.Add(this.LBText);
+            this.PNText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PNText.Location = new System.Drawing.Point(0, 26);
+            this.PNText.Name = "PNText";
+            this.PNText.Padding = new System.Windows.Forms.Padding(3);
+            this.PNText.Size = new System.Drawing.Size(294, 58);
+            this.PNText.TabIndex = 10;
+            // 
+            // PNTitle
+            // 
+            this.PNTitle.Controls.Add(this.LBTitle);
+            this.PNTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PNTitle.Location = new System.Drawing.Point(0, 0);
+            this.PNTitle.Name = "PNTitle";
+            this.PNTitle.Padding = new System.Windows.Forms.Padding(3);
+            this.PNTitle.Size = new System.Drawing.Size(294, 26);
+            this.PNTitle.TabIndex = 9;
+            // 
+            // PNIcon
+            // 
+            this.PNIcon.Controls.Add(this.PBIcon);
+            this.PNIcon.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PNIcon.Location = new System.Drawing.Point(3, 3);
+            this.PNIcon.Name = "PNIcon";
+            this.PNIcon.Size = new System.Drawing.Size(60, 84);
+            this.PNIcon.TabIndex = 9;
             // 
             // ToastForm
             // 
@@ -105,8 +155,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
             this.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
-            this.ClientSize = new System.Drawing.Size(360, 80);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(360, 90);
+            this.Controls.Add(this.PNMain);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ToastForm";
@@ -117,8 +167,11 @@
             this.TopMost = true;
             this.Load += new System.EventHandler(this.ToastForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PBIcon)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.PNMain.ResumeLayout(false);
+            this.PNContainer.ResumeLayout(false);
+            this.PNText.ResumeLayout(false);
+            this.PNTitle.ResumeLayout(false);
+            this.PNIcon.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -131,6 +184,10 @@
         private System.Windows.Forms.Timer TMHide;
         private System.Windows.Forms.Timer TMHideAnim;
         private System.Windows.Forms.Timer TMShowAnim;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel PNMain;
+        private System.Windows.Forms.Panel PNIcon;
+        private System.Windows.Forms.Panel PNContainer;
+        private System.Windows.Forms.Panel PNText;
+        private System.Windows.Forms.Panel PNTitle;
     }
 }
---
+++ Azylee.Utils/Tests/Test.Toast/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Azylee.WinformSkin.FormUI.Toast;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Azylee.WinformSkin.FormUI.Toast.ToastForm;
+
+namespace Test.Toast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToastForm.Display("中车四方终端准入系统提示", "系统自动检测发现故障，由于准入系统认证组件工作异常，导致无法正常访问网络，需要进行尝试修复，请点击此提示系统自动修复，修复成功后请尝试登录。", ToastType.info, null, 10);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ToastForm.Display("中车四方终端准入系统提示", "一二三四五六七八九十一二三四五六七八", ToastType.info, null, 10);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToastForm.Display("中车四方终端准入系统提示", "一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五", ToastType.info, null, 10);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ToastForm.Display("中车四方终端准入系统提示", "一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六", ToastType.info, null, 10);
+        }
+
+        string str = "六";
+        private void button5_Click(object sender, EventArgs e)
+        {
+            str = "一二三四五六七八九十";
+            str = str + str + str + str + str + str + str + str + str + str + str
+                 + str + str + str + str + str + str + str + str + str + str
+                 + str + str + str + str + str + str + str + str + str + str;
+            ToastForm.Display("中车四方终端准入系统提示", str, ToastType.info, null, 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ToolTipIcon icon = ToolTipIcon.Info;
+            icon = ToolTipIcon.Error;
+            notifyIcon1.ShowBalloonTip(5, "123wrefdasdfgf", str, icon);
+        }
+    }
+}
