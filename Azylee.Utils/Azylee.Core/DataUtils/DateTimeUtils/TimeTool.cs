0f1afbecf6bdf8128ddc6e7d07aac39021b818df
yuzhengyang <inc@live.cn>
2019-04-21 17:14:52 +0800

    添加时间差计算

---
+++ Azylee.Utils/Azylee.Core/DataUtils/DateTimeUtils/TimeTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Azylee.Core.DataUtils.DateTimeUtils
+{
+    /// <summary>
+    /// 时间计算工具类
+    /// </summary>
+    public static class TimeTool
+    {
+        /// <summary>
+        /// 时间与当前时间差
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static double TimeDiffSec(DateTime dt)
+        {
+            return (dt - DateTime.Now).TotalSeconds;
+        }
+    }
+}
