8e628b7b8dbf2e00ed177f17534519e59c60df01
yuzhengyang <inc@live.cn>
2020-07-25 21:58:56 +0800

    增加打开网址方法

---
+++ Azylee.Utils/Azylee.Core/WindowsUtils/BrowserUtils/BrowserTool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Azylee.Core.WindowsUtils.BrowserUtils
+{
+    public class BrowserTool
+    {
+        public static bool OpenUrl(string url)
+        {
+            bool _flag = false;
+            if (BrowserSelector.ModernBrowser(out string browser))
+            {
+                try
+                {
+                    Process.Start(browser, $"{url}");
+                    _flag = true;
+                }
+                catch { }
+            }
+
+            if (_flag == false)
+            {
+                try
+                {
+                    Process.Start($"{url}");
+                    _flag = true;
+                }
+                catch { }
+            }
+            return _flag;
+        }
+    }
+}
