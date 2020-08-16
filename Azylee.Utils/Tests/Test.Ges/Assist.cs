2ba53d28dd4435cc950d22d6cfa6ac27f89f7907
yuzhengyang <inc@live.cn>
2020-08-16 22:16:28 +0800

    测试及引用调整

---
+++ Azylee.Utils/Tests/Test.Ges/Assist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Ges
+{
+    public class Assist<T>
+    {
+        public Assist()
+        {
+            Type tp = typeof(T);
+            Console.WriteLine($"{tp.ToString()}");
+        }
+    }
+}
---
+++ Azylee.Utils/Tests/Test.Ges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Ges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assist<User> assist = new Assist<User>();
+
+            Console.Read();
+        }
+    }
+}
