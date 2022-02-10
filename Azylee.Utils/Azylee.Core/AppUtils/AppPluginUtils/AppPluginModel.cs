49e160d6dc5de7d672b62a11ca7c626732c72ce7
于正洋 <yuzhyn@163.com>
2022-02-10 18:23:22 +0800

    app轻量插件化工具

---
+++ Azylee.Utils/Azylee.Core/AppUtils/AppPluginUtils/AppPluginModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Azylee.Core.AppUtils.AppPluginUtils
+{
+    public class AppPluginModel
+    {
+        /// <summary>
+        /// 插件名称
+        /// </summary>
+        public string PluginName { get; set; }
+        /// <summary>
+        /// 插件描述信息
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public string Version { get; set; }
+        /// <summary>
+        /// 更新建议（0-无，1-实验，2-测试，3-稳定，4-强制）
+        /// </summary>
+        public int UpdateAdvice { get; set; }
+    }
+}
