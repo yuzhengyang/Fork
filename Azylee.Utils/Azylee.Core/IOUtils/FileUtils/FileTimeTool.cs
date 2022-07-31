d6702ec256ad51e0f132deeba62e37a0241c57c7
yuzhengyang <inc@live.cn>
2022-07-31 12:10:07 +0800

    文件创建时间工具

---
+++ Azylee.Utils/Azylee.Core/IOUtils/FileUtils/FileTimeTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Azylee.Core.IOUtils.FileUtils
+{
+    public static class FileTimeTool
+    {
+        /// <summary>
+        /// 检查文件创建至此已过n分钟
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static double pastMinutes(string fileName)
+        {
+            double result = -1;
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    FileInfo fi = new FileInfo(fileName);
+                    result = (DateTime.Now - fi.CreationTime).TotalMinutes;
+                }
+                catch (Exception e) { }
+            }
+            return result;
+        }
+    }
+}
