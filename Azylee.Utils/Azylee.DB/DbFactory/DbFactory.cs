64944e913e8e22b9463035f50e53daeb3745ce13
yuzhengyang <inc@live.cn>
2022-08-03 15:06:07 +0800

    数据库链接工具

---
+++ Azylee.Utils/Azylee.DB/DbFactory/DbFactory.cs
@@ -0,0 +1,27 @@
+﻿using Azylee.Core.AppUtils.AppConfigUtils.AppConfigModels;
+using Azylee.Core.DbUtils.DbInterface;
+using Azylee.Core.DbUtils.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azylee.DB.DbFactory
+{
+    public class DbFactory
+    {
+        //public static IDatabaseHelper create(AppConfigDbItem config, string database = null)
+        //{
+        //    switch (config.DbType())
+        //    {
+        //        case DatabaseType.PostgreSQL:
+        //            return new PgSqlDatabaseHelper(config, database);
+
+        //        case DatabaseType.Mysql:
+        //        default:
+        //            return new MySqlDatabaseHelper(config, database);
+        //    }
+        //}
+    }
+}
